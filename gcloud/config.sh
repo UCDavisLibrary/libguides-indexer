ec98ec1e1b8c202dffd1d8ce20c504249e3fb17e
Justin Merz <jrmerz@gmail.com>
2025-02-10 13:30:11 -0800

    fixes for cork-kube build

--- gcloud/config.sh
+++ gcloud/config.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
 PROJECT_ID=digital-ucdavis-edu
-GCR_PROJECT_ID=ucdlib-pubreg
-
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
-CONTAINER_NAME=libguides-indexer-$BRANCH_NAME
-IMAGE=gcr.io/$GCR_PROJECT_ID/$CONTAINER_NAME
+IMAGE_NAME=libguides-indexer
+IMAGE=us-west1-docker.pkg.dev/digital-ucdavis-edu/website/$IMAGE_NAME:$BRANCH_NAME
 DEPLOYMENT_NAME=$CONTAINER_NAME
 SUBSCRIPTION_NAME=libguides-indexer-$BRANCH_NAME
 TOPIC_NAME=libguides-indexer-worker-$BRANCH_NAME
--- gcloud/deploy.sh
+++ gcloud/deploy.sh
@@ -5,16 +5,16 @@ cd $ROOT_DIR/..
 
 set -e
 
-# source ./gcloud/config.sh
+source ./gcloud/config.sh
 
-# gcloud config set project $PROJECT_ID
-# gcloud builds submit --tag $IMAGE
+# cork-kube build gcb \
+#   -p libguides-indexer \
+#   -v $BRANCH_NAME
 
-
-
-gcloud beta run deploy $DEPLOYMENT_NAME-server \
+gcloud beta run deploy $IMAGE_NAME-$BRANCH_NAME-server \
   --image $IMAGE \
   --platform managed \
+  --project $PROJECT_ID \
   --memory=1Gi \
   --region=us-central1 \
   --allow-unauthenticated \
@@ -24,9 +24,10 @@ gcloud beta run deploy $DEPLOYMENT_NAME-server \
   --set-env-vars=BRANCH=$BRANCH_NAME \
   --update-secrets=SERVICE_URL=libguides-indexer-main-server-url:latest,WORKER_URL=libguides-indexer-main-worker-url:latest,LIBGUIDES_SECRET_JSON=libguides-api-client-id-secret:latest
 
-gcloud beta run deploy $DEPLOYMENT_NAME-worker \
+gcloud beta run deploy $IMAGE_NAME-$BRANCH_NAME-worker \
   --image $IMAGE \
   --platform managed \
+  --project $PROJECT_ID \
   --memory=1Gi \
   --region=us-central1 \
   --allow-unauthenticated \
